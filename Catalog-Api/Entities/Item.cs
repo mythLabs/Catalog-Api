0a0f6a59bce1d7b4bc0672f5dc331e632bdd1076
mythLabs <amitbisht744@gmail.com>
2021-07-17 11:16:38 +0530

    Added Record type entity

---
+++ Catalog-Api/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_Api.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+
+        public int Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
